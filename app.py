058c04943dc61bf7ec0a2f36ac63d25b95679574
hala09 <hala09292001@gmail.com>
2023-03-17 05:00:07 -0700

    initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import function_data as function
+import requests
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+
+    return render_template("index.html")
---
+++ function_data.py
@@ -0,0 +1,9 @@
+import requests
+import urllib
+
+
+# define a function - get_book_data
+
+# define a function - get_book_safe
+
+# define a function - get_book_cover
